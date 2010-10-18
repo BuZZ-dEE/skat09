ff1ac72cc2045a136d91770e0d104070f37e9302
BuZZ-dEE <deezzub@gmail.com>
2010-10-19 01:21:22 +0200

    system look & feel

--- src/skat09/Main.java
+++ src/skat09/Main.java
@@ -2,19 +2,20 @@ package skat09;
 
 import java.io.IOException;
 
-import skat09.Tisch;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import skat09.test.interfaces.IAusgabe;
 import skat09.test.interfaces.IController;
 import skat09.ui.CLIausgabe;
 import skat09.ui.GUIausgabe;
 
-
-
 /**
- * Die Main Klasse dient dem Programmstart, sowie dem Initialisieren der f&uuml;r
- * das Programm ben&ouml;tigten Objekte.
+ * Die Main Klasse dient dem Programmstart, sowie dem Initialisieren der
+ * f&uuml;r das Programm ben&ouml;tigten Objekte.
  * 
- * @author Ann-Christine Kycler, Sebastian Schlatow, Mathias Stoislow, Martin Bruhns
+ * @author Ann-Christine Kycler, Sebastian Schlatow, Mathias Stoislow, Martin
+ *         Bruhns
  * @version 03.07.2009
  */
 public class Main {
@@ -24,33 +25,47 @@ public class Main {
 	 * 
 	 * @param args
 	 *            &Uuml;bergegebene Parameter
-	 * @throws IOException 
+	 * @throws IOException
 	 * 
 	 */
 	public static void main(String[] args) throws IOException {
 
 		Tisch tisch = new Tisch();
 
-		
-			String s = null;
-			IAusgabe ausgabe;
+		String s = null;
+		IAusgabe ausgabe;
+		try {
+			s = args[0];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			s = "";
+		}
+
+		if (s.compareTo("-nogui") == 0) {
+			ausgabe = new CLIausgabe(tisch);
+		} else {
+
 			try {
-				s = args[0]; }
-			catch(ArrayIndexOutOfBoundsException e) {
-				s = "";
+				// Set System L&F
+				UIManager.setLookAndFeel(UIManager
+						.getSystemLookAndFeelClassName());
+			} catch (UnsupportedLookAndFeelException e) {
+				// handle exception
+			} catch (ClassNotFoundException e) {
+				// handle exception
+			} catch (InstantiationException e) {
+				// handle exception
+			} catch (IllegalAccessException e) {
+				// handle exception
 			}
-			
-			if (s.compareTo("-nogui") == 0) {
-				ausgabe = new CLIausgabe(tisch);
-			} else {
-				ausgabe = new GUIausgabe(tisch);
-			}
-			
-			IController controller = new Controller(tisch, ausgabe);
-			if (ausgabe instanceof GUIausgabe) {
-				controller.warte();
-			}
-			controller.spiel();
+
+			ausgabe = new GUIausgabe(tisch);
+		}
+
+		IController controller = new Controller(tisch, ausgabe);
+		if (ausgabe instanceof GUIausgabe) {
+			controller.warte();
+		}
+		controller.spiel();
 
 	}
 
