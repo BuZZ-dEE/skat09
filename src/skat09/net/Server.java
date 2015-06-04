0ab1caf69b9329934459e0514b6e366ab977ed95
Sebastian Schlatow <deezzub@gmail.com>
2015-06-04 23:22:01 +0200

    Add serial version uid

--- src/skat09/net/Server.java
+++ src/skat09/net/Server.java
@@ -4,13 +4,14 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 public class Server extends UnicastRemoteObject implements IServer {
+	
+	private static final long serialVersionUID = -8604752504494313592L;
 
 	protected Server() throws RemoteException {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
 	public String sayHello() throws RemoteException {
 		// TODO Auto-generated method stub
 		return null;
