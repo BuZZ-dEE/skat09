8eed48229bf1f50022dd984cd6ef6fc2429b2333
Sebastian Schlatow <deezzub@gmail.com>
2015-06-15 22:48:07 +0200

    Server class

---
+++ src/skat09/net/Server.java
@@ -0,0 +1,70 @@
+package skat09.net;
+
+
+import io.netty.bootstrap.ServerBootstrap;
+
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * Skat09 server
+ * 
+ * @since 15.06.2015 20:18:27
+ * 
+ * @author Sebastian Schlatow <ssc@openmailbox.org>
+ */
+public class Server {
+
+    private int port;
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public void run() throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap(); // (2)
+            b.group(bossGroup, workerGroup)
+             .channel(NioServerSocketChannel.class) // (3)
+             .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
+                 @Override
+                 public void initChannel(SocketChannel ch) throws Exception {
+                     ch.pipeline().addLast(new ServerHandler());
+                 }
+             })
+             .option(ChannelOption.SO_BACKLOG, 128)          // (5)
+             .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
+
+            // Bind and start to accept incoming connections.
+            ChannelFuture f = b.bind(port).sync(); // (7)
+
+            // Wait until the server socket is closed.
+            // In this example, this does not happen, but you can do that to gracefully
+            // shut down your server.
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    /**
+     * Start the server.
+     * 
+     * @throws Exception
+     * 
+     * @since 15.06.2015 20:38:26
+     * 
+     * @author Sebastian Schlatow <ssc@openmailbox.org>
+     */
+    public void start() throws Exception {
+    	run();
+    }
+}
