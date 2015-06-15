8347fbb748b71664a2a5385725841ca663ffe9ab
Sebastian Schlatow <deezzub@gmail.com>
2015-06-15 20:13:37 +0200

    Rename to ServerHandler

--- src/skat09/net/ServerHandler.java
+++ src/skat09/net/ServerHandler.java
@@ -1,21 +1,26 @@
 package skat09.net;
 
 import io.netty.buffer.ByteBuf;
-
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
 
 /**
  * Handles a server-side channel.
  */
-public class Server extends ChannelHandlerAdapter { // (1)
+public class ServerHandler extends ChannelHandlerAdapter { // (1)
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
         // Discard the received data silently.
-        ((ByteBuf) msg).release(); // (3)
+        try {
+            // Do something with msg
+        	((ByteBuf) msg).release(); // (3)
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
     }
-
+    
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
         // Close the connection when an exception is raised.
