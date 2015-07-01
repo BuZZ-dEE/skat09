3d3e8b136117d03f527c699f98619d99218e3155
Sebastian Schlatow <deezzub@gmail.com>
2015-07-01 23:58:17 +0200

    Add settings dialog

---
+++ src/skat09/ui/gui/SettingsDialog.java
@@ -0,0 +1,21 @@
+package skat09.ui.gui;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Settings dialog for things like language etc.
+ * 
+ * @since 20.06.2015 21:33:28
+ * 
+ * @author Sebastian Schlatow <ssc@openmailbox.org>
+ *
+ */
+public class SettingsDialog extends JOptionPane {
+	
+	private static final long serialVersionUID = 4589989271147381375L;
+	
+	public SettingsDialog() {
+		
+	}
+	
+}
